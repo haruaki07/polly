2a56ca09fc03eaf3045b3afed375e0871b879dfd
haruaki07 <misbahulk2236@gmail.com>
2023-08-01 15:02:29 +0700

    feat: landing page

--- src/web/App.svelte
+++ src/web/App.svelte
@@ -1,11 +1,34 @@
 <script>
-  import { AppBar } from "@skeletonlabs/skeleton"
+  import { AppBar, AppShell } from "@skeletonlabs/skeleton"
   import Logo from "./lib/components/Logo.svelte"
+  import Container from "./lib/components/Container.svelte"
 </script>
 
-<AppBar>
-  <a class="flex items-center gap-x-2" href=".">
-    <Logo />
-    <h4 class="h4 font-bold">Polly</h4>
-  </a>
-</AppBar>
+<div class="sticky top-0 z-10">
+  <AppBar gridColumns="grid-cols-1">
+    <Container center>
+      <a class="inline-flex items-center gap-x-2" href=".">
+        <Logo />
+        <h4 class="h4 font-bold">Polly</h4>
+      </a>
+    </Container>
+  </AppBar>
+</div>
+
+<div class="px-4">
+  <Container center className="flex flex-col items-center">
+    <div class="inline-flex flex-col mt-24 items-center">
+      <input
+        type="number"
+        class="input input-no-arrow"
+        placeholder="Enter code here"
+      />
+      <button type="button" class="btn variant-filled mt-4 max-w-[128px]"
+        >Join Event</button
+      >
+      <button type="button" class="anchor text-xs mt-2">
+        or create event
+      </button>
+    </div>
+  </Container>
+</div>
---
+++ src/web/lib/components/Container.svelte
@@ -0,0 +1,10 @@
+<script>
+  import clsx from "clsx"
+
+  export let center = false
+  export let className = ""
+</script>
+
+<div class={clsx("max-w-5xl w-full", center && "mx-auto", className)}>
+  <slot />
+</div>
